158300d5ac3a5efaafb31bc3c15f4a27fc016c23
LeonidCher <4ls2005@mail.ru>
2024-12-18 01:02:49 +0400

    header

--- vite-project/src/Components/Header.tsx
+++ vite-project/src/Components/Header.tsx
@@ -1,10 +1,20 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
+import headerLogo from "../../public/icon/boulwarDepTxt.png";
+import "./header.css";
 
-const Header = ()=>{
-    return (
-        <>fdngksmgokmgobmposmdgfbomgobm aaayyyeee
-        <Outlet></Outlet>
-        </>
-    )
-}
-export default Header
+const Header = () => {
+  return (
+    <>
+      <div className="container">
+        <header className="header">
+          <img src={headerLogo} alt="" className="headerLogo" />
+          <p className="headertxt">тур</p>
+          <p className="headertxt">музыка</p>
+          <p className="headertxt">мерч</p>
+        </header>
+      </div>
+      <Outlet></Outlet>
+    </>
+  );
+};
+export default Header;
